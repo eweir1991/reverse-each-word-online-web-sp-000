01edc98aaec5e0570840db519c9327263d59504a
Erik Weir <erik.weir@key.me>
2020-08-12 20:34:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
-  orginial_array = string.split(" ")
+  orginial_array = string.split("Hello there, and how are you?")
   return_array []
   orginial_array.each do |string|
     return_array << string.reverse
   end
-  return_array.join(" ")
+  return_array.join("Hello there, and how are you?")
 end
